d896027cca2d5466f375b3f8ebf5af292ba6388d
Renat626 <kasymov.2002@mail.ru>
2019-11-17 03:11:40 +0500

    dz by php6

---
+++ dz6/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <title>document</title>
+  </head>
+  <body>
+    <ul>
+      <li><a href="/html/index2.php">Калькулятор</a></li>
+      <li><a href="/html/gallary/index3.php">Галерея</a></li>
+    </ul>
+  </body>
+</html>
